1cf28ff82f9508b4ae5c7bb6641f69528b24a71d
Malte S. Stretz <mss@apache.org>
2011-07-09 13:33:30 +0200

    Did you know that download.macromedia.com supports HTTPS?

--- getflashplugin64.sh
+++ getflashplugin64.sh
@@ -8,7 +8,7 @@ mkdir -p ~/.mozilla/plugins
 cd ~/.mozilla/plugins
 
 url="http://labs.adobe.com/downloads/flashplayer10_square.html"
-url=$(wget -O- $url | sed -e '/_64bit_linux_/!d;s,^.*"\(http://download.macromedia.com/[^"]*\)".*$,\1,')
+url=$(wget -O- $url | sed -e '/_64bit_linux_/!d;s,^.*"http:\(//download.macromedia.com/[^"]*\)".*$,https:\1,')
 test "$url"
 
 file=$(basename $url)
